8015f96b73964dcbbc3288c1b7d99c6bff466530
Karl Seguin <karl@openmymind.net>
2012-06-06 10:59:32 +0800

    fix

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,35 +1,34 @@
 class Context
   attr_accessor :key
-  
+
   def initialize
     @key = BSON::ObjectId.new.to_s
-    @@database = Store.connection.db('learn_1')
   end
-  
-  def to_mongo  
-    @@database
+
+  def to_mongo
+    Store.connection.db('learn_1')
   end
-  
+
   def to_collection(name)
     to_mongo.collection(to_real_name(name))
   end
-  
+
   def database_names
     [@key]
   end
-  
+
   def host
     'arrakis'
   end
-  
+
   def collection_names
     ['unicorns']
   end
-  
+
   def real_collection_names
     collection_names.map{|c| to_real_name(c)}
   end
-  
+
   def to_real_name(name)
     name + '_' + @key
   end
